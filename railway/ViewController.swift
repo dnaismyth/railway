2b5a3284eba09bb33a7b347d5b86816ee1aea15c
Dayna Naismyth <naismythdayna@gmail.com>
2017-01-28 06:33:36 -0800

    Initial Commit

---
+++ railway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  railway
+//
+//  Created by Dayna Naismyth on 2017-01-28.
+//  Copyright © 2017 Dayna Naismyth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
