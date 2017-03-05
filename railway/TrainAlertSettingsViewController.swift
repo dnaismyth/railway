c48486e3f1f7061e8c418d0ed7138e3d7d023fa4
Dayna Naismyth <naismythdayna@gmail.com>
2017-03-04 17:42:22 -0800

    Fix report train crossing to update firebase data

--- railway/TrainAlertSettingsViewController.swift
+++ railway/TrainAlertSettingsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class TrainAlertSettingsViewController: UIViewController {
     
@@ -17,6 +18,8 @@ class TrainAlertSettingsViewController: UIViewController {
     var trainCrossingId : Int = -1
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    let rootRef = FIRDatabase.database().reference()    // reference to the firebase database
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,7 +59,32 @@ class TrainAlertSettingsViewController: UIViewController {
         let params : [String : AnyObject] = [:]
         let url : String = Constants.API.reportTrainCrossing.replacingOccurrences(of: "id", with: String(trainCrossingId))
         PostRequest().jsonPost(postUrl: url, token: access_token!, body: params) { (dictionary) in
-            print("Finished reporting")
+            if(dictionary["operationType"] != nil){
+                let operationType : String = dictionary["operationType"] as! String
+                if(operationType == "CREATE"){
+                    print("Report created")
+                    self.updateFirebaseNotificationCount(trainCrossingId: trainCrossingId)   // increment notification count
+                } else if (operationType == "NO_CHANGE"){
+                    print("Too many reports")
+                }
+
+            } else {
+                // send error message
+            }
         }
+    }
+    
+    private func updateFirebaseNotificationCount(trainCrossingId : Int){
+        let key = "traincrossing_".appending(String(trainCrossingId))
+        let trainCrossingData = rootRef.child("traincrossing")
+        let childNode = trainCrossingData.child(key)    // find a child node by the train crossing id
+        childNode.observeSingleEvent(of: .value, with: { snapshot in
+            if(snapshot.hasChild("is_active") && snapshot.hasChild("notification_count")){
+                var notificationCount : Int = snapshot.childSnapshot(forPath: "notification_count").value as! Int
+                notificationCount += 1
+                childNode.child("notification_count").setValue(notificationCount)
+            }
+        })
+        
     }
 }
